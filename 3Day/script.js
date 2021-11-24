75db894b42faa558cf0bfaae9c113a643aca8a69
riska_rahmadhani <riskarahmadhani422@gmail.com>
2021-11-24 08:11:30 +0700

    first commit

---
+++ 3Day/script.js
@@ -0,0 +1,11 @@
+// scroll reveal
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "15px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(" .col-md-4, .row ", {
+  interval: 200,
+});
---
+++ js/script.js
@@ -0,0 +1,11 @@
+// scroll reveal
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "15px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(" .content, .col-md-4, .col-md-6 ", {
+  interval: 200,
+});
